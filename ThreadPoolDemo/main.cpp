2b63e6ac5317ef1845bca624bd40e8e2f88c9ef2
liu6326sc <Ltian@Ltian-2.local>
2014-10-20 11:23:06 +0800

    线程池
    线程池

---
+++ ThreadPoolDemo/main.cpp
@@ -0,0 +1,53 @@
+//
+//  main.cpp
+//  ThreadPoolDemo
+//
+//  Created by Ltian on 14/10/20.
+//  Copyright (c) 2014年 Ltian. All rights reserved.
+//
+#include <unistd.h>
+#include <iostream>
+#include "CThreadPool.h"
+#include <iostream>
+#include <stdlib.h>
+#include "TestTask.h"
+int main()
+{
+	TestTask taskObj;
+	
+	char szTmp[] = "this is the first thread running";
+	taskObj.SetData((void*)szTmp);
+    CThreadPool* threadPool = CThreadPool::sharedTheadPoll();
+    
+    /*测试方法*/
+    /************************************************************************************/
+    for(int i = 0; i < 20; i++)
+    {
+        //sleep(1);
+        threadPool->AddTask(&taskObj);
+    }
+    sleep(1);
+	for(int i = 0; i < 20; i++)
+	{
+        sleep(1);
+		threadPool->AddTask(&taskObj);
+	}
+    /************************************************************************************/
+	
+	while(1)
+	{
+		printf("there are still %d tasks need to handle/n", threadPool->getTaskSize());
+		if (threadPool->getTaskSize() == 0)
+		{
+			if (threadPool->StopAll() == -1)
+			{	
+				printf("Now I will exit from main/n");
+				exit(0);
+			}
+		}
+		sleep(20000);
+	}
+	
+	return 0;
+}
+
